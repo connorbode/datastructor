29146ea6ddecd1b4ce9bc169fa50f4f7869d705b
Connor Bode <connor.bode@gmail.com>
2015-01-30 13:32:34 -0500

    added operations for sequences

--- app/models/sequence/index.js
+++ app/models/sequence/index.js
@@ -2,7 +2,9 @@ module.exports = function (app) {
   var Schema = app.db.Schema;
   var Sequence = new Schema({
     data: Schema.Types.Mixed,
+    operations: [ Schema.Types.Mixed ],
     type: { type: Number, ref: 'DataStructure' }
   });
+
   return app.db.model('Sequence', Sequence);
 };
