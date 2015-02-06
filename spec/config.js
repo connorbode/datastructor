ff7615a8430eef63fb9d370ec989267ad7e6d8c2
Connor Bode <connor.bode@gmail.com>
2015-02-06 17:37:43 -0500

    configure mockgoose for testing without db connection

--- spec/config.js
+++ spec/config.js
@@ -4,27 +4,38 @@
  * This file is loaded before Mocha tests are run
  */
 
+var mockgoose = require('mockgoose');
+
+before(function (done) {
+  process.env.GITHUB_CLIENT_ID = 'test';
+  process.env.GITHUB_CLIENT_SECRET = 'test';
+
+  global.app = {};
+  require('../app/config')(app);
+  mockgoose(app.db);
+  app.tasks = require('../app/tasks')(app);
+  app.controllers = require('../app/controllers')(app);
+  app.models = require('../app/models')(app);
+  done();
+});
+
 before(function (done) {
   global.chai = require('chai');
   global.assert = chai.assert;
   global.expect = chai.expect;
   global.should = chai.should();
+  global.supertest = require('supertest');
+  global.nock = require('nock');
   global._ = require('lodash');
-  process.env.GITHUB_CLIENT_ID = 'test';
-  process.env.GITHUB_CLIENT_SECRET = 'test';
   done();
 });
 
 beforeEach(function (done) {
-  global.app = {};
-  require('../app/config')(global.app);
+  mockgoose.reset();
   done();
 });
 
-afterEach(function (done) {
-  global.app.db.connection.close(function () {
-    app.db.models = {};
-    app.db.modelSchemas = {};
-    done();
-  });
+after(function (done) {
+  app.db.connection.close();
+  done();
 });
