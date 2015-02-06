a7c7fe5a77eff6dd6c712b649a61a312b4e1459f
Connor Bode <connor.bode@gmail.com>
2015-02-06 14:51:51 -0500

    renamed identity type to identity provider

--- app/models/identity/index.js
+++ app/models/identity/index.js
@@ -2,12 +2,12 @@ module.exports = function (app) {
   var Schema = app.db.Schema;
   var Identity = new Schema({
     token: String,
-    type: String
+    provider: String
   });
 
   var IdentityModel = app.db.model('Identity', Identity);
 
-  IdentityModel.schema.path('type').validate(function (value) {
+  IdentityModel.schema.path('provider').validate(function (value) {
     return app.sso[value] === undefined ? false : true;
   });
 
--- app/models/identity/index.spec.js
+++ app/models/identity/index.spec.js
@@ -9,7 +9,7 @@ describe('app.models.identity', function () {
   it('allows identities to be created using enabled sso providers', function (done) {
     var providers = _.keys(app.sso);
     _.forEach(providers, function (value, index) {
-      Identity.create({token: 'test', type: value}, function (err, identity) {
+      Identity.create({token: 'test', provider: value}, function (err, identity) {
         assert.isNull(err);
         if (index === providers.length - 1) {
           done();
@@ -19,7 +19,7 @@ describe('app.models.identity', function () {
   });
 
   it('does not allow identities to be created using non-enabled sso providers', function (done) {
-    Identity.create({token: 'test', type: 'google'}, function (err, identity) {
+    Identity.create({token: 'test', provider: 'google'}, function (err, identity) {
       assert.isNotNull(err);
       done();
     });
