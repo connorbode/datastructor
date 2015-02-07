4ce81b6b039c614b09cb2442e67696421590c59c
Connor Bode <connor.bode@gmail.com>
2015-02-06 19:50:40 -0500

    sectioned off api routes

--- app/controllers/api/sessions/create/index.js
+++ app/controllers/api/sessions/create/index.js
@@ -7,9 +7,9 @@ module.exports = function (app) {
     app.controllers.checkParams(req, res);
 
     // authenticate with the provider supplied
-    app.tasks.auth[req.body.provider](req.body.code, function (err, token) {
+    app.tasks.auth[req.body.provider](req.body.code, function (err, token, email) {
       app.controllers.error(err, res);
-      app.tasks.login({ token: token, provider: req.body.provider }, function (err) {
+      app.tasks.login(req.body.provider, token, email, function (err) {
         app.controllers.error(err, res);
       });
     });
