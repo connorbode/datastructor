2e2129dc7246fb07b9bfc21f4439060825eab8f8
Connor Bode <connor.bode@gmail.com>
2015-02-06 23:59:26 -0500

    added login task

--- app/tasks/login/index.js
+++ app/tasks/login/index.js
@@ -1,5 +1,45 @@
 module.exports = function (app) {
-  return function (params, callback) {
-    
+  return function (provider, token, email, callback) {
+    var Account = app.models.Account;
+    var Identity = app.models.Identity;
+    var identities;
+    var identity;
+    var account;
+    var accountData;
+    Account
+      .findOne({ email: email })
+      .populate({
+        path: 'identities',
+        model: 'Identity',
+        match: {
+          provider: provider
+        }
+      })
+      .exec(function (err, account) {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        if (!account) {
+          callback('email not found');
+          return;
+        }
+
+        if (account.identities.length === 0) {
+          callback('identity not found');
+          return;
+        }
+
+        account.identities[0].token = token;
+        account.identities[0].save(function (err) {
+          if (err) {
+            callback(err);
+            return;
+          }
+
+          callback();
+        });
+      });
   };
 };
---
+++ app/tasks/login/index.spec.js
@@ -0,0 +1,46 @@
+describe('app.tasks.login', function () {
+  it('fails if the email does not exist in the system', function (done) {
+    app.tasks.login('provider', 'token', 'email', function (err) {
+      assert.isDefined(err);
+      assert.equal(err, 'email not found');
+      done();
+    });
+  });
+
+  it('fails if the provider that was used to log in is not attached to the account', function (done) {
+    var identity;
+    var account;
+    identity = new app.models.Identity({ token: 'fake', provider: 'github' });
+    identity.save(function (err) {
+      account = new app.models.Account({ email: 'fake@email.com', identities: [ identity._id ] });
+      account.save(function (err) {
+        app.tasks.login('google', 'token', 'fake@email.com', function (err) {
+          assert.isDefined(err);
+          assert.equal(err, 'identity not found');
+          done();
+        });
+      });
+    });
+  });
+
+  it('updates the token with the associated identity if login is successful', function (done) {
+    var identity;
+    var account;
+    identity = new app.models.Identity({ token: 'oldtoken', provider: 'github' });
+    identity.save(function (err) {
+      account = new app.models.Account({ email: 'fake@email.com', identities: [ identity._id ] });
+      account.save(function (err) {
+        app.tasks.login('github', 'newtoken', 'fake@email.com', function (err) {
+          assert.isUndefined(err);
+          app.models.Account
+            .findOne({ email: 'fake@email.com' })
+            .populate('identities')
+            .exec(function (err, account) {
+              assert.equal(account.identities[0].token, 'newtoken');
+              done();
+            });
+        });
+      })
+    });
+  });
+});
