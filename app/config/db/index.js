9ea25d9b16f7cc54da10bd47d347bf7a0b30c6fe
Connor Bode <connor.bode@gmail.com>
2015-02-12 13:48:39 -0500

    hide db error logs while testing

--- app/config/db/index.js
+++ app/config/db/index.js
@@ -1,4 +1,7 @@
 module.exports = function (app) {
   app.db = require('mongoose');
   app.db.connect('mongodb://localhost:27017/datastructor');
+  app.db.connection.on('error', function (err) {
+    // log, maybe?
+  });
 };
