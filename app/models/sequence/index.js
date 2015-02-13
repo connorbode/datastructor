9eba5378328b2ebdc16cb73619ce29347e248111
Connor Bode <connor.bode@gmail.com>
2015-02-13 16:09:44 -0500

    added a name field to the sequence model

--- app/models/sequence/index.js
+++ app/models/sequence/index.js
@@ -1,6 +1,7 @@
 module.exports = function () {
   var Schema = app.db.Schema;
   var Sequence = new Schema({
+    name: String,
     data: Schema.Types.Mixed,
     operations: [ Schema.Types.Mixed ],
     type: { type: Schema.Types.ObjectId, ref: 'DataStructure' }
