f25eb72d8678e0110ef81fc223931dd349b15c80
Connor Bode <connor.bode@gmail.com>
2015-02-13 16:09:35 -0500

    added a name field to the datastructure model

--- app/models/data-structure/index.js
+++ app/models/data-structure/index.js
@@ -1,6 +1,7 @@
 module.exports = function () {
   var Schema = app.db.Schema;
   var DataStructure = new Schema({
+    name: String,
     validation: Schema.Types.Mixed,
     operations: [{ type: Schema.Types.ObjectId, ref: 'Operation' }]
   });
