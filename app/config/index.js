5bb955244f970a0bdd3f65d498a2d86f23ec5cfa
Connor Bode <connor.bode@gmail.com>
2015-02-20 12:56:06 -0500

    check that all the appropriate env vars have been set

--- app/config/index.js
+++ app/config/index.js
@@ -1,4 +1,24 @@
 module.exports = function () {
+
+  // check that the appropriate environment  variables have been set
+  var env = [
+    'GITHUB_CLIENT_ID',
+    'GITHUB_CLIENT_SECRET'
+  ];
+
+  var unset = "";
+
+  env.forEach(function (e) {
+    if (!process.env[e]) {
+      unset += e + ", ";
+    }
+  });
+
+  if (unset) {
+    unset = unset.trim(/, $/, "");
+    throw "The following environment variables must be set: " + unset;
+  }
+
   require('./routing')();
   require('./db')();
   require('./sso')();
