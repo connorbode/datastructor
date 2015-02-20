a318cca985900b9824d4a5742965b5bee4889093
Connor Bode <connor.bode@gmail.com>
2015-02-20 13:05:43 -0500

    serve static files

--- app/config/routing/index.js
+++ app/config/routing/index.js
@@ -3,8 +3,10 @@ var validator = require('express-validator');
 var resource = require('express-resource');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 module.exports = function () {
+  var public = path.join(__dirname + "/../../public");
   app.routing = express();
   app.routing.use(bodyParser.json());
   app.routing.use(validator({
@@ -21,4 +23,5 @@ module.exports = function () {
     saveUninitialized: true,
     store: app.routing.sessionStore
   }));
+  app.routing.use('/public', express.static(public));
 };
