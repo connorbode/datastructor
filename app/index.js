27630d9b93add1f92fb1734d5343a0bd4a007be6
Connor Bode <connor.bode@gmail.com>
2015-02-25 20:30:15 -0500

    redirect all traffic to index.html

--- app/index.js
+++ app/index.js
@@ -9,6 +9,9 @@ app.tasks = require('./tasks');
 app.models = require('./models');
 app.controllers = require('./controllers')();
 
+app.routing.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 app.routing.listen(port);
 
 console.log(('datastructor running on port ' + port).green);
