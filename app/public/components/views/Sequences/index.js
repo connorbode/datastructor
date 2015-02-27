31e315d2b30b7eb0f9407546e3cb7f3d15e84597
Connor Bode <connor.bode@gmail.com>
2015-02-27 14:05:18 -0500

    list sequences page

--- app/public/components/views/Sequences/index.js
+++ app/public/components/views/Sequences/index.js
@@ -1,9 +1,30 @@
-var React  = require('react');
+var React     = require('react');
 var UserStore = require('../../../stores/UserStore');
+var $         = require('jquery');
 
 module.exports = React.createClass({
+  handleAddSequence: function () {
+    console.log('add sequence!');
+  },
+
+  componentDidMount: function () {
+    $('#add-sequence-btn').on('click', this.handleAddSequence);
+  },
+
   render: function () {
     return (
+      <div className="sequence-wrapper">
+        <div className="sequences">
+          <h1>
+            <span>sequences</span>
+            <i id="add-sequence-btn" className="fa fa-plus-circle"></i>
+          </h1>
+          <ul>
+
+          </ul>
+          <div className="no-sequences">You don't have any sequences!</div>
+        </div>
+      </div>
     );
   }
 });
