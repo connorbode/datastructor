9c4a850c4935adda555da7b0a4a47d68037a4fb4
Connor Bode <connor.bode@gmail.com>
2015-02-27 20:17:50 -0500

    set up new sequence page

--- app/public/components/views/SequenceNew/index.js
+++ app/public/components/views/SequenceNew/index.js
@@ -1,9 +1,26 @@
-var React = require('react');
+var React          = require('react');
+var SequenceEditor = require('../../partials/SequenceEditor');
+var Dropdown      = require('react-select');
 
 module.exports = React.createClass({
+  _onSelect: function () {
+
+  },
+
   render: function () {
+    var options = [
+      { value: 'one', label: 'One' },
+      { value: 'two', label: 'Two' }
+    ];
     return (
-      <h1>New Sequence!</h1>
+      <div className="table-center-wrapper">
+        <div className="table-center sequence-new">
+          <h1>add a sequence</h1>
+          <input type="text" placeholder="sequence title" />
+          <Dropdown name="hi" options={options} onChange={this._onSelect} placeholder="sequence structure" />
+          <button>create!</button>
+        </div>
+      </div>
     );
   }
 });
