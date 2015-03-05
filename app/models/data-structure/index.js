0689a79fc99e28b8192d9c377d1552536f7b4bd9
Connor Bode <connor.bode@gmail.com>
2015-03-05 10:44:13 -0500

    add an initialization function to the datastructure model

--- app/models/data-structure/index.js
+++ app/models/data-structure/index.js
@@ -3,7 +3,8 @@ module.exports = function () {
   var DataStructure = new Schema({
     name: String,
     validation: Schema.Types.Mixed,
-    operations: [{ type: Schema.Types.ObjectId, ref: 'Operation' }]
+    operations: [{ type: Schema.Types.ObjectId, ref: 'Operation' }],
+    initialization: String
   });
   return app.db.model('DataStructure', DataStructure);
 };
