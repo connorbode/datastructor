7cdb6993d3282d2670d7e9626d6ab79545dc0318
Connor Bode <connor.bode@gmail.com>
2015-03-20 19:36:27 -0400

    added a code editor to edit datastructure

--- app/public/components/views/DataStructureEdit/index.js
+++ app/public/components/views/DataStructureEdit/index.js
@@ -1,11 +1,29 @@
 var React           = require('react/addons');
 var StructureStore  = require('../../../stores/StructureStore');
+var CodeMirror      = require('codemirror');
+require('codemirror/mode/javascript/javascript');
+
+var _editorElem;
+var _containerElem;
+var _codeMirrorObj;
 
 module.exports = React.createClass({
   getInitialState: function () {
     return {
       structure: {}
     };
+  },
+
+  setCodeMirrorSize: function () {
+    var offsetTop;
+    var containerHeight;
+    var containerWidth;
+
+    offsetTop       = parseInt(window.getComputedStyle(_editorElem).getPropertyValue('top').replace(/px/, ''));
+    containerHeight = _containerElem.scrollHeight;
+    containerWidth  = _containerElem.scrollWidth;
+
+    _codeMirrorObj.setSize(containerWidth, containerHeight - 2 * offsetTop);
   },
 
   handleStructureChange: function () {
@@ -15,7 +33,17 @@ module.exports = React.createClass({
   },
 
   componentDidMount: function () {
+
+    _editorElem = document.getElementById('editor');
+    _containerElem = document.getElementById('editor-container');
+    _codeMirrorObj = CodeMirror(_editorElem, {
+      lineNumbers:  true,
+      theme:        'monokai'
+    });
+
     StructureStore.addChangeListener(this.handleStructureChange);
+
+    this.setCodeMirrorSize();
   },
 
   componentWillUnmount: function () {
@@ -24,7 +52,10 @@ module.exports = React.createClass({
 
   render: function () {
     return (
-      <h1>{this.state.structure.name}</h1>
+      <div id="editor-container" className="structure-edit">
+        <h1>{this.state.structure.name}</h1>
+        <div id="editor" className="editor"></div>
+      </div>
     );
   }
 });
