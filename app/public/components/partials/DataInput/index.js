e14d1ad1b854c1a4e2c71eb83c5a7da7999c0dbc
Connor Bode <connor.bode@gmail.com>
2015-03-27 10:16:44 -0400

    get the real value from the data input

--- app/public/components/partials/DataInput/index.js
+++ app/public/components/partials/DataInput/index.js
@@ -5,12 +5,36 @@ var _val = {};
 module.exports = React.createClass({
 
   /**
+   * The all data in this component is stored 
+   * in the value key of an object.  This is
+   * to ensure that parameters are always
+   * passed by reference instead of by value
+   * (hence preserving all relationships).
+   *
+   * This function iterates through an object,
+   * removing all the "value" keys.
+   */
+  getRealVal: function (val) {
+    var realVal;
+    if (Array.isArray(val.value)) {
+      realVal = val.value.map(function (v) {
+        return this.getRealVal(v);
+      }.bind(this));
+    } else {
+      realVal = val.value;
+    }
+    return realVal;
+  },
+
+  /**
    * Calls the change handler supplied
    * to this component.
    */
   emitChange: function () {
+    var val;
     if (this.props.onChange) {
-      this.props.onChange(_val);
+      val = this.getRealVal(_val);
+      this.props.onChange(val);
     }
   },
 
