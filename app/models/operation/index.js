30cc83b840d74c377d9aaea993e209a790a67d63
Connor Bode <connor.bode@gmail.com>
2015-03-29 17:29:17 -0400

    delete operation and data structure models
    #10

--- app/models/operation/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-  var Schema = app.db.Schema;
-  var Operation = new Schema({
-    name:       String,
-    validation: Schema.Types.Mixed,
-    operation:  String
-  });
-  return app.db.model('Operation', Operation);
-};
