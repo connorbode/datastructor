127185c186acc4eed0f701959068efa5d4760c60
Connor Bode <connor.bode@gmail.com>
2015-03-29 20:32:43 -0400

    modify structureconstants
    #10

--- app/public/constants/StructureConstants/index.js
+++ app/public/constants/StructureConstants/index.js
@@ -1,10 +1,11 @@
 module.exports = {
-  CREATE_SUCCESS: 'structure.create.success',
-  CREATE_ERROR:   'structure.create.error',
-  LIST_SUCCESS:   'structure.list.success',
-  LIST_ERROR:     'structure.list.error',
-  GET_SUCCESS:    'structure.get.success',
-  GET_ERROR:      'structure.get.error',
-  UPDATE_SUCCESS: 'structure.update.success',
-  UPDATE_ERROR:   'structure.update.error'
+  types: {
+    ARRAY: 'array'
+  },
+  viewers: {
+    ARRAY: 'ArrayViewer'
+  },
+  editors: {
+    ARRAY: 'ArrayEditor'
+  }
 };
