3dd961a9d72cffad899c0219e70be327c8ef3b4e
Connor Bode <connor.bode@gmail.com>
2015-03-30 20:04:47 -0400

    loading the array view
    #9

---
+++ app/public/components/views/SequenceEdit/index.js
@@ -0,0 +1,40 @@
+var React               = require('react');
+var SequenceStore       = require('../../../stores/SequenceStore');
+var StructureConstants  = require('../../../constants/StructureConstants');
+
+var SequenceEditor;
+var editors = {
+  ArrayEditor: require('../../partials/ArrayEditor')
+};
+
+module.exports = React.createClass({
+
+  getInitialState: function () {
+    return {
+      sequence: null
+    };
+  },
+
+  handleSequenceLoaded: function () {
+    var state = this.state;
+    var editorFileName;
+    var editorFilePath;
+    state.sequence = SequenceStore.getSequence();
+    editor = StructureConstants.editors[state.sequence.type];
+    SequenceEditor = editors[editor];
+    this.setState(state);
+  },
+
+  componentDidMount: function () {
+    SequenceStore.addChangeListener(this.handleSequenceLoaded);
+  },
+
+  componentWillUnmount: function () {
+    SequenceStore.removeChangeListener(this.handleSequenceLoaded);
+  },
+
+  render: function () {
+    var editor = this.state.sequence ? <SequenceEditor /> : <div>Loading</div>;
+    return (<div>{editor}</div>);
+  }
+});
