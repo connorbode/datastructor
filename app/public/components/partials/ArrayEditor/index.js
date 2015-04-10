27166871341fbb5a676d87c4ac3c33ea3e8877cc
Connor Bode <connor.bode@gmail.com>
2015-04-10 10:32:09 -0400

    o_O

--- app/public/components/partials/ArrayEditor/index.js
+++ app/public/components/partials/ArrayEditor/index.js
@@ -9,111 +9,108 @@ function updateSequence () {
 };
 
 var ArrayOperations = {
-  initialization: {
-    validation: {
-      type:   'integer',
-      label:  'initial array size'
-    },
+  initialization: function (viewport, data) {
 
-    operation: function (viewport, data) {
+  },
 
-      // initialize empty array
-      var i = 0;
-      _array = [];
-      for (i; i < data; i += 1) {
-        _array.push({ value: null });
-      }
+  createArray: function (viewport, data) {
 
-      // add the groups for each node
-      var nodes = viewport
-        .selectAll('g')
-        .data(_array)
-        .enter()
-        .append('g');
-
-      // add the background circle
-      nodes
-        .append('circle')
-        .attr('fill', '#ccc')
-        .attr('cx', function (d, i) {
-          return (i + 1) * 50;
-        })
-        .attr('r', '20')
-        .attr('stroke', '#aaa')
-        .attr('stroke-width', '0')
-        .style('cursor', 'pointer')
-        .on('mouseover', function (d) {
-          d3.select(this)
-            .attr('stroke-width', '2');
-        })
-        .on('mouseout', function (d) {
-          d3.select(this)
-            .attr('stroke-width', '0');
-        });
-
-      // add the value of the node
-      nodes
-        .append('text')
-        .attr('fill', '#aaa')
-        .style('font-size', '20px')
-        .attr('x', function (d, i) {
-          return ((i + 1) * 50);
-        })
-        .attr('y', 10)
-        .attr('text-anchor', 'middle')
-        .text(function (d) {
-          return (d.value || '_');
-        })
-        .style('cursor', 'pointer')
-        .on('mouseover', function (d) {
-          d3.select(this)
-            .attr('fill', 'black');
-        })
-        .on('mouseout', function (d) {
-          d3.select(this)
-            .attr('fill', '#aaa');
-        })
-        .on('click', function (d) {
-          var elem = d3.select('.edit-node-value')
-            .classed('open', true)
-            .style('top', d3.event.clientY)
-            .style('left', d3.event.clientX);
-
-          var node = elem.node();
-
-          node.focus();
-          node.value = '';
-        });
-
-      // add opening bracket
-      viewport
-        .append('text')
-        .attr('fill', 'black')
-        .attr('y', '12')
-        .style('font-size', '40px')
-        .text('[');
-
-      // add closing bracket
-      viewport
-        .append('text')
-        .attr('fill', 'black')
-        .attr('y', '12')
-        .attr('x', ((_array.length + 1) * 50) - 10)
-        .style('font-size', '40px')
-        .text(']');
-
-      // add a blur event for the node editor
-      d3.select('.edit-node-value')
-        .on('blur', function (d) {
-          d3.select(this)
-            .classed('open', false);
-        })
-        .on('keydown', function (d) {
-          if (d3.event.keyCode === 13) {
-            d3.select(this).classed('open', false);
-          }
-        });
+    // initialize empty array
+    var i = 0;
+    _array = [];
+    for (i; i < data; i += 1) {
+      _array.push({ value: null });
     }
+
+    // add the groups for each node
+    var nodes = viewport
+      .selectAll('g')
+      .data(_array)
+      .enter()
+      .append('g');
+
+    // add the background circle
+    nodes
+      .append('circle')
+      .attr('fill', '#ccc')
+      .attr('cx', function (d, i) {
+        return (i + 1) * 50;
+      })
+      .attr('r', '20')
+      .attr('stroke', '#aaa')
+      .attr('stroke-width', '0')
+      .style('cursor', 'pointer')
+      .on('mouseover', function (d) {
+        d3.select(this)
+          .attr('stroke-width', '2');
+      })
+      .on('mouseout', function (d) {
+        d3.select(this)
+          .attr('stroke-width', '0');
+      });
+
+    // add the value of the node
+    nodes
+      .append('text')
+      .attr('fill', '#aaa')
+      .style('font-size', '20px')
+      .attr('x', function (d, i) {
+        return ((i + 1) * 50);
+      })
+      .attr('y', 10)
+      .attr('text-anchor', 'middle')
+      .text(function (d) {
+        return (d.value || '_');
+      })
+      .style('cursor', 'pointer')
+      .on('mouseover', function (d) {
+        d3.select(this)
+          .attr('fill', 'black');
+      })
+      .on('mouseout', function (d) {
+        d3.select(this)
+          .attr('fill', '#aaa');
+      })
+      .on('click', function (d) {
+        var elem = d3.select('.edit-node-value')
+          .classed('open', true)
+          .style('top', d3.event.clientY)
+          .style('left', d3.event.clientX);
+
+        var node = elem.node();
+
+        node.focus();
+        node.value = '';
+      });
+
+    // add opening bracket
+    viewport
+      .append('text')
+      .attr('fill', 'black')
+      .attr('y', '12')
+      .style('font-size', '40px')
+      .text('[');
+
+    // add closing bracket
+    viewport
+      .append('text')
+      .attr('fill', 'black')
+      .attr('y', '12')
+      .attr('x', ((_array.length + 1) * 50) - 10)
+      .style('font-size', '40px')
+      .text(']');
+
+    // add a blur event for the node editor
+    d3.select('.edit-node-value')
+      .on('blur', function (d) {
+        d3.select(this)
+          .classed('open', false);
+      })
+      .on('keydown', function (d) {
+        if (d3.event.keyCode === 13) {
+          d3.select(this).classed('open', false);
+        }
+      });
   }
 };
 
--- app/public/components/partials/SequenceEditor/index.js
+++ app/public/components/partials/SequenceEditor/index.js
@@ -18,25 +18,6 @@ module.exports = React.createClass({
     };
   },
 
-  addStep: function () {
-    var state = this.state;
-    state.step = ADD_STEP_NAME;
-    this.setState(state);
-  },
-
-  loadInitializationStep: function () {
-    var state = this.state;
-    state.step = INITIALIZATION_STEP_NAME;
-    this.setState(state);
-  },
-
-  handleInitializationDataChange: function (data) {
-    var state = this.state;
-    state.sequence.data = data;
-    this.setState(state);
-    SequenceActions.update(state.sequence);
-  },
-
   handleSequenceChange: function () {
     var state = this.state;
     state.sequence = SequenceStore.getSequence();
@@ -55,37 +36,7 @@ module.exports = React.createClass({
     var steps = this.state.sequence ? this.state.sequence.operations : [];
     var operations = this.state.structure ? this.state.structure.operations : [];
     var cx = React.addons.classSet;
-    var stepEditorClass = cx({
-      'step-editor':  true,
-      'hide':         !this.state.step
-    });
-    var initializationStepClass = cx({
-      'selected':   this.state.step === INITIALIZATION_STEP_NAME,
-      'clickable':  this.state.step !== INITIALIZATION_STEP_NAME,
-      'step':       true
-    });
-    var initializationContentClass = cx({
-      'step-content': true,
-      'show':         this.state.step === INITIALIZATION_STEP_NAME
-    });
-    var initializationInput = {
-      validation: this.state.structure.initialization.validation || {},
-      data:       this.state.sequence.data || {},
-      onChange:   this.handleInitializationDataChange
-    };
-    var addStepClass = cx({
-      'step':       true,
-      'selected':   this.state.step === ADD_STEP_NAME,
-      'clickable':  this.state.step !== ADD_STEP_NAME
-    });
-    var addStepContentClass = cx({
-      'step-content': true,
-      'show':         this.state.step === ADD_STEP_NAME
-    });
-    var addStepNameClass = cx({
-      'step-name':  true,
-      'hide':       this.state.step === ADD_STEP_NAME
-    });
+
     var viewerProps = {
       structure:  this.state.structure,
       sequence:   this.state.sequence
@@ -96,27 +47,16 @@ module.exports = React.createClass({
         <SequenceViewer {...viewerProps} />
         <div className="sequence-editor">
           <ul className="steps">
-            <li className={initializationStepClass} onClick={this.loadInitializationStep}>
-              <div className="step-name">{INITIALIZATION_STEP_NAME}</div>
-              <div className={initializationContentClass}>
-                <DataInput {...initializationInput} />
-              </div>
+            <li className="step">
+              <div className="step-name">Initialization</div>
             </li>
             {steps.map(function (step) {
               return (
-                <li>
+                <li className="step">
                   <div className="step-name">{step.name}</div>
                 </li>
               );
             })}
-            <li className={addStepClass} onClick={this.addStep}>
-              <div className={addStepNameClass}>
-                <i className="fa fa-plus"></i>
-              </div>
-              <div className={addStepContentClass}>
-                Hi there this is content
-              </div>
-            </li>
           </ul>
         </div>
       </div>
