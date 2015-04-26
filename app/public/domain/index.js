74e7050562eb6b796223ff7840ac4b8b1fffb5ab
Connor Bode <connor.bode@gmail.com>
2015-04-25 23:17:25 -0400

    wooopz

--- app/public/domain/index.js
+++ app/public/domain/index.js
@@ -2,11 +2,11 @@ var uuid = require('node-uuid');
 var DomainObject = require('./DomainObject');
 
 module.exports = {
+  Collection:     require('./Collection'),
   genId:          DomainObject.genId,
   getObject:      DomainObject.getObject,
   Link:           require('./Link'),
   LinkableNode:   require('./LinkableNode'),
   Node:           require('./Node'),
-  NodeCollection: require('./NodeCollection'),
   Tree:           require('./Tree')
 };
